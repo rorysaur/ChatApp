ff8eba714ac86b0ebccdeabe86231a800a308bc0
Rory Ou <ou.rory@gmail.com>
2014-01-13 14:58:29 -0800

    add working chat form

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,14 +1,45 @@
 (function (root) {
   var ChatApp = root.ChatApp = root.ChatApp || {};
 
-  var socket = io.connect();
-  socket.on("news", function (data) {
-    console.log(data);
-    var chat = new ChatApp.Chat(socket);
-    chat.sendMessage("HI IM IN THE CHATROOM");
-  });
+  var installSocketHandlers = function () {
+    ChatApp.socket.on("news", function (data) {
+      console.log("Socketed.");
+      installKeyHandler();
+      ChatApp.chat.sendMessage("Say something.");
+    });
 
-  socket.on("new-message", function (msg) {
-    $("div#messages").append(msg);
-  })
+    ChatApp.socket.on("new-message", function (msg) {
+      $('div#messages').append(msg + '<br>');
+    });
+  }
+
+  // var installSubmitHandler = function () {
+  //   $('#speak').on('submit', function (event) {
+  //       event.preventDefault();
+  //       var msg = $(this).find('input').val();
+  //       ChatApp.chat.sendMessage(msg);
+  //   });
+  // };
+
+  var installKeyHandler = function () {
+    $('input').on('keydown', function (event) {
+      if (event.which === 13) {
+        event.preventDefault();
+        var msg = $(this).val();
+        ChatApp.chat.sendMessage(msg);
+        $(this).val("");
+      }
+    })
+  }
+
+  var start = ChatApp.start = function () {
+    installSocketHandlers();
+  }
+
 })(this);
+
+$(function () {
+  ChatApp.socket = io.connect();
+  ChatApp.chat = new ChatApp.Chat(ChatApp.socket);
+  ChatApp.start();
+});
