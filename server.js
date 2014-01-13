ae702bd7acbbdb93482355ea26031e60553b8c6a
Rory Ou <ou.rory@gmail.com>
2014-01-13 13:38:18 -0800

    add ability for server and browser to connect via Socket.IO

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var http = require("http");
-// var fs = require("fs");
-// var path = require("path");
 var mime = require("mime");
 var static = require("node-static");
+var chatServer = require("./lib/chat_server");
 
 var fileServer = new static.Server("./public");
 
@@ -18,6 +17,9 @@ var server = http.createServer(function (request, response) {
     });
   }).resume();
 });
+
+chatServer.createChat(server);
+// console.log(chatServer);
 
 server.listen(8080);
 
